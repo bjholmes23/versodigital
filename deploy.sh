b11bf246a0fe4502b4ba1d226801d8652439424a
Timo Saikkonen <timo.saikkonen@gmail.com>
2015-07-02 22:37:20 +0800

    Fix brackets in error handlers

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ if git diff-index --quiet HEAD --; then
     git checkout gh-pages
     git pull
     git merge master
-    [ $? -gt 0 ] && ( echo "Merge failed"; exit 1; )
+    [ $? -gt 0 ] && { echo "Merge failed"; exit 1; }
     npm install
     bower install
     gulp build
-    [ $? -gt 0 ] && ( echo "Build failed"; exit 1; )
+    [ $? -gt 0 ] && { echo "Build failed"; exit 1; }
     npm prune --production
     git add -f .
     git commit -m "Build $(date +"%D %T")"
