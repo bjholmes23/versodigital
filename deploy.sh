6125c58ed82c46f0be3810e760fde4c1cfe12e2c
Timo Saikkonen <timo.saikkonen@gmail.com>
2015-07-02 22:33:01 +0800

    Deploy script error handling

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,15 @@ if git diff-index --quiet HEAD --; then
     git checkout gh-pages
     git pull
     git merge master
+    [ $? -gt 0 ] && ( echo "Merge failed"; exit 1; )
     npm install
     bower install
     gulp build
+    [ $? -gt 0 ] && ( echo "Build failed"; exit 1; )
     npm prune --production
     git add -f .
     git commit -m "Build $(date +"%D %T")"
+    git checkout master
 else
     echo "Uncommitted changes, exiting."
 fi
