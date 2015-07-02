7019be30bf8824bc1f0cb9e268dda8268a380926
Timo Saikkonen <timo.saikkonen@gmail.com>
2015-07-02 22:21:26 +0800

    Install dependencies on deployment

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,10 @@ if git diff-index --quiet HEAD --; then
     git checkout gh-pages
     git pull
     git merge master
+    npm install
+    bower install
     gulp build
+    npm prune --production
     git add -f .
     git commit -m "Build $(date +"%D %T")"
 else
